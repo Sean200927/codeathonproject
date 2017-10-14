81eb2f87807d91af7807510589f28242adc446dc
Dmitri <dmitrismith5@gmail.com>
2017-10-14 16:16:47 -0500

    Set up sign up screen

--- app/src/main/java/codeathon/ku/mentr/LoginActivity.java
+++ app/src/main/java/codeathon/ku/mentr/LoginActivity.java
@@ -3,6 +3,7 @@ package codeathon.ku.mentr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,8 +13,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    //Check if the users credentials are correct
     public void AttemptLogin(View view)
     {
+        //Tracks if the username/password combo was correct
+        boolean isValid = false;
+        //Tracks whether the user is a mentor
+        boolean isMentor = false;
 
+        EditText usernameText = (EditText) findViewById(R.id.usernameEditText);
+        String username = usernameText.getText().toString();
+
+        EditText passText = (EditText) findViewById(R.id.passwordEditText);
+        String password = passText.getText().toString();
+        /* Should compare username to database - if username exists, check password.
+         * If password is correct, grab mentor property and use it to continue to correct page
+         */
+        if(isValid)
+        {
+            if(isMentor)
+            {
+                //Goto mentor management screen
+            }
+
+
+            //Goto mentor selection
+        }
     }
 }
--- app/src/main/java/codeathon/ku/mentr/SignupActivity.java
+++ app/src/main/java/codeathon/ku/mentr/SignupActivity.java
@@ -2,6 +2,8 @@ package codeathon.ku.mentr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void BeginSignup(View view)
+    {
+        //Tracks if the username/password combo was correct
+        boolean isValid = false;
+        //Tracks whether the user is a mentor
+        boolean isMentor = false;
+
+        EditText usernameText = (EditText) findViewById(R.id.emailEditText);
+        String username = usernameText.getText().toString();
+
+        EditText passText = (EditText) findViewById(R.id.passwordEditText);
+        String password = passText.getText().toString();
+
+        EditText passConfirmText = (EditText) findViewById(R.id.passConfirmEditText);
+        String passwordConfirm = passConfirmText.getText().toString();
+        /* Should compare username to database - if username exists, check password.
+         * If password is correct, grab mentor property and use it to continue to correct page
+         */
+        if(isValid)
+        {
+            if(isMentor)
+            {
+                //Goto mentor management screen
+            }
+
+
+            //Goto mentor selection
+        }
     }
 }
