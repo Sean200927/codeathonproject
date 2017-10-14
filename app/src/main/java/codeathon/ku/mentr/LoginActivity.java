e8f005a00c902b061e92d3fcf3ebe4f32121f2fc
Dmitri <dmitrismith5@gmail.com>
2017-10-14 12:47:05 -0500

    Test

---
+++ app/src/main/java/codeathon/ku/mentr/LoginActivity.java
@@ -0,0 +1,13 @@
+package codeathon.ku.mentr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/codeathon/ku/mentr/SplashActivity.java
@@ -0,0 +1,21 @@
+package codeathon.ku.mentr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void GotoLogin(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
