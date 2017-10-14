5c966fcfcfecd4d2ac84919573f2d0e8c91e345f
JASavage42 <32405289+JASavage42@users.noreply.github.com>
2017-10-14 14:58:56 -0500

    Add files via upload

---
+++ app/src/main/java/codeathon/ku/mentr/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package codeathon.ku.mentr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void GotoLogin(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void GotoSignup(View view)
+    {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
