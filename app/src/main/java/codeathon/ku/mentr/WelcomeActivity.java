e87602b0a18bcaaaa9894c38157649a42287df72
Dmitri <dmitrismith5@gmail.com>
2017-10-14 13:59:07 -0500

    Gateway can now go into login and signup

--- app/src/main/java/codeathon/ku/mentr/WelcomeActivity.java
+++ app/src/main/java/codeathon/ku/mentr/WelcomeActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class SplashActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
